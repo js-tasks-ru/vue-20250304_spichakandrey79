5654a06064543fc8e5522d2d58ad5b0e142327d9
Martensit2019 <martensit2019@yandex.ru>
2025-03-19 19:51:46 +0300

    Resolve tasks 03-10 MeetupView and 03-20 UiClock

--- 03-components/10-MeetupView/MeetupView.js
+++ 03-components/10-MeetupView/MeetupView.js
@@ -12,31 +12,33 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
   },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    }
+  },     
 
   template: `
     <div>
-
-      <!-- Обложка митапа -->
-
+      <MeetupCover :image="meetup.image" :title="meetup.title"></MeetupCover>
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h2>Описание</h2>
-
-            <!-- Описание митапа -->
-
+            <MeetupDescription :description="meetup.description" />
             <h2>Программа</h2>
-
-            <!-- Программа митапа -->
-            <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-            <UiAlert></UiAlert>
-
+            <MeetupAgenda v-if="meetup.agenda?.length" :agenda="meetup.agenda"></MeetupAgenda>
+            <UiAlert v-else> Программа пока пуста…</UiAlert>
           </div>
           <div class="meetup__aside">
-
-            <!-- Краткая информация о митапе -->
-
+            <MeetupInfo :place="meetup.place" :organizer="meetup.organizer" :date="meetup.date"></MeetupInfo>
             <div class="meetup__aside-buttons"></div>
           </div>
         </div>
--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const currentDate = ref(new Date())
 
-  template: `<div class="clock">10:12:02</div>`,
+    const currentTime = computed(() => currentDate.value.toLocaleTimeString(navigator.language, { timeStyle: 'medium' }))
+
+    const interval = setInterval(() => {
+      currentDate.value = new Date()
+    }, 1000) 
+
+    onUnmounted(() => {
+      clearInterval(interval);
+    });
+
+    return {
+      currentTime
+    }
+  },
+
+  template: `<div class="clock">{{ currentTime }}</div>`,
 })
