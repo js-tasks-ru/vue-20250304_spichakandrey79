745c983d376f762bcb1c8f443705a03131020bcd
Martensit2019 <martensit2019@yandex.ru>
2025-03-27 19:52:44 +0300

    Resolve 05 10 MeetupView-TS

--- 05-ts/10-MeetupView-TS/MeetupInfo.vue
+++ 05-ts/10-MeetupView-TS/MeetupInfo.vue
@@ -1,23 +1,13 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
 import { UiIcon } from '@shgk/vue-course-ui'
 
-const props = defineProps({
-  organizer: {
-    type: String,
-    required: true,
-  },
+const props =defineProps<{
+  organizer: string
+  place: string
+  date: number
+  }>()
 
-  place: {
-    type: String,
-    required: true,
-  },
-
-  date: {
-    type: Number,
-    required: true,
-  },
-})
 
 const isoDate = computed(() => new Date(props.date).toISOString().slice(0, 10))
 const localDate = computed(() =>
