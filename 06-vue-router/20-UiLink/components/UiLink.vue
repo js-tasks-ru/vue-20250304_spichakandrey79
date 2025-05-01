cf5d6676ac7a2579aed8f87dc92872dc83fddd2c
Martensit2019 <martensit2019@yandex.ru>
2025-05-01 20:14:50 +0300

    UiLink

--- 06-vue-router/20-UiLink/components/UiLink.vue
+++ 06-vue-router/20-UiLink/components/UiLink.vue
@@ -1,10 +1,23 @@
-<script setup>
+<script setup lang="ts">
+import { computed } from 'vue'
+
 // Вместо <span> должен быть <RouterLink> или <a>
 // Используйте динамический компонент <component :is="...">
+
+const props = defineProps<{
+  to?: string | {}
+  href?: string
+}>()
+
+const link = computed(() => props.href ? {to: props.to} : {href: props.href})
 </script>
 
 <template>
-  <span class="link" tabindex="0">Link</span>
+  <component :is="props.to ? 'router-link' : 'a'" :link="link" class="link">
+    <slot />
+  </component>
+
+  <!-- <span class="link" tabindex="0">Link</span> -->
 </template>
 
 <style scoped>
