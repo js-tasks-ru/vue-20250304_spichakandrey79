3679d638827365204977ee5dc6e5873e571cb3e7
Martensit2019 <martensit2019@yandex.ru>
2025-05-02 13:19:25 +0300

    UiLink

--- 06-vue-router/20-UiLink/components/UiLink.vue
+++ 06-vue-router/20-UiLink/components/UiLink.vue
@@ -1,26 +1,18 @@
-<script setup>
-import { computed } from 'vue';
-const props = defineProps({
-  to: {
-    type: [String, Object],
-    required: false,
-  },
-  href: {
-    type: String,
-    required: false,
-  },
-});
-const componentType = computed(() => {
-  return props.to ? 'RouterLink' : 'a';
-});
-const componentProps = computed(() => {
-  return props.to ? { to: props.to } : { href: props.href };
-});
+<script setup lang="ts">
+import { computed } from 'vue'
+import type {RouteLocationRaw } from 'vue-router'
+// Вместо <span> должен быть <RouterLink> или <a>
+// Используйте динамический компонент <component :is="...">
+const props = defineProps < {
+  to?: RouteLocationRaw
+  href?: string
+} > ()
+const link = computed(() => props.to ? { to: props.to } : { href: props.href })
 </script>
 
 <template>
-  <component :is="componentType" v-bind="componentProps" class="link">
-    <slot>Link</slot>
+  <component :is="props.to ? 'router-link' : 'a'" v-bind="link" class="link">
+    <slot />
   </component>
 </template>
 
